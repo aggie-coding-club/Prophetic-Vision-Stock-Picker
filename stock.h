069e0d98851199614972f366f674d350657061d1
Varish Kongara <varish.kongara@gmail.com>
2024-11-12 19:40:24 -0600

    Added variables and calculated variables from deliverable 11/16. Did not change constructor.

--- stock.h
+++ stock.h
@@ -9,20 +9,43 @@ private:
     std::string companyName;
     std::string symbol;
     double price;
-    // double assets;
-    // double liabilities;
-    // double grossProfit;
-    // double longTermDebt;
-    // double shareholderEquity;
-    // double netIncome;
+    double assets;
+    double liabilities;
+    double inventory;
+    double longTermDebt;
+    double shareholderEquity;
+    double netIncome;
 public:
+    //constructors
     stock(std::string companyName, std::string symbol);
+
+    //getters
     std::string getCompanyName();
     std::string getSymbol();
     double getPrice();
+    double getAssets();
+    double getLiabilities();
+    double getInventory();
+    double getLongTermDebt();
+    double getShareholderEquity();
+    double getNetIncome();
+
+    //setters
     void setCompanyName(std::string companyName);
     void setSymbol(std::string symbol);
-    void setPrice(int price);
+    void setPrice(double price);
+    void setAssets(double assets);
+    void setLiabilities(double liabilities);
+    void setInventory(double inventory);
+    void setLongTermDebt(double longTermDebt);
+    void setShareholderEquity(double shareholderEquity);
+    void setNetIncome(double netIncome);
 
+    //calculated variables
+    double currentRatio();
+    double quickRatio();
+    double DebtToEquityRatio();
+    double ROE();
+    double ROA();
 };
 #endif
